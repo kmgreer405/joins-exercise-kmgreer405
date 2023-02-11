57827bb6b5b67bbcdbd4f4c2904aefee28838ba0
Kevin Greer <kmgreer405@hotmail.com>
2023-02-11 10:56:32 -0500

    Through question 2

--- scripts/joins_exercise.sql
+++ scripts/joins_exercise.sql
@@ -12,11 +12,19 @@ LIMIT 1;
 --Lowest grossing movie worldwide is "Semi-Tough" released in 1977 with a gross of 37,187,139
 
 -- 2. What year has the highest average imdb rating?
+SELECT release_year, ROUND(AVG(imdb_rating), 2) AS avg_imdb
+FROM specs
+INNER JOIN rating
+ON specs.movie_id = rating.movie_id
+GROUP BY release_year
+ORDER BY AVG(imdb_rating) DESC
+LIMIT 1;
+
+--1991 has the highest average imdb rating at 7.45
 
 -- 3. What is the highest grossing G-rated movie? Which company distributed it?
 
--- 4. Write a query that returns, for each distributor in the distributors table, the distributor name and the number of movies associated with that distributor in the movies 
--- table. Your result set should include all of the distributors, whether or not they have any movies in the movies table.
+-- 4. Write a query that returns, for each distributor in the distributors table, the distributor name and the number of movies associated with that distributor in the movies table. Your result set should include all of the distributors, whether or not they have any movies in the movies table.
 
 -- 5. Write a query that returns the five distributors with the highest average movie budget.
 
